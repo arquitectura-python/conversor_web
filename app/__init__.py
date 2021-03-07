27cd462165f2ff502c91df034247a8a0f5e80a1c
angel mondragon <amondrave54@gmail.com>
2021-03-07 15:01:22 -0500

    Creacion de metodo de subida y descarga de ficheros

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-
+import os
 
 from flask import Flask
 
-app = Flask(__name__)
+directorio = os.getcwd()
 
+app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = directorio + '/app/static/uploads'
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF", "BMP"]
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
+import os
+
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, send_file, abort,send_from_directory, url_for
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/upload-image", methods = ["POST"])
+def recibir_imagen():
+    if request.method == "POST":
+        if request.files:
+            extension = request.form.get("extension")
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            print(image)
+            print(extension)
+            return redirect(url_for('descargar_imagen',nombre_imagen = image.filename))
+
+
+@app.route("/download-image/<nombre_imagen>")
+def descargar_imagen(nombre_imagen):
+    try:
+        return send_from_directory(app.config["IMAGE_UPLOADS"], filename=nombre_imagen, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+              
