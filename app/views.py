9308abcc7caa4d9b2f6a3c689a0427cdb988398b
Danilo23 <daniloquintero0908@gmail.com>
2021-03-08 09:07:43 -0500

    Restricción de extensiones
    No se permite subir extensiones diferentes de las solicitadas

--- app/views.py
+++ app/views.py
@@ -3,11 +3,24 @@ import os
 from app import app
 from app.metodos import convertir_imagen
 from flask import render_template, request, redirect, send_file, abort,send_from_directory, url_for
+from werkzeug.utils import secure_filename
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 @app.route("/upload-image", methods = ["POST"])
 def recibir_imagen():
@@ -15,11 +28,16 @@ def recibir_imagen():
         if request.files:
             extension = request.form.get("extension")
             image = request.files["image"]
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            print(image.filename)
-            print(extension)
-            nuevo_nombre =  convertir_imagen(image,extension)
-            return redirect(url_for('descargar_imagen',nombre_imagen = nuevo_nombre))
+            if image.filename == "":
+                print("No hay nada seleccionado")
+                return redirect(request.url)
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+                print(image.filename)
+                print(extension)
+                nuevo_nombre =  convertir_imagen(image,extension)
+                return redirect(url_for('descargar_imagen',nombre_imagen = nuevo_nombre))
 
 
 @app.route("/download-image/<nombre_imagen>")
