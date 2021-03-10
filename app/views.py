0231e60fae84843c0bed7e82aa86e43381895d9b
adriana05 <43660492+adriana05@users.noreply.github.com>
2021-03-09 21:22:17 -0500

    carpeta views.py

--- app/views.py
+++ app/views.py
@@ -6,23 +6,29 @@ from flask import render_template, request, redirect, send_file, abort,send_from
 from werkzeug.utils import secure_filename
 
 
-@app.route("/")
+@app.route("/") #se define la ruta por defecto 
 def index():
     error = ""
-    return render_template("index.html", error = request.args.get('error'))
+    return render_template("index.html", error = request.args.get('error')) # se renderiza el archo index html de nustra carpeta template
 
-def allowed_image(filename):
+def allowed_image(filename):# metodo para saber si la extension del archivo es valida 
 
-    if not "." in filename:
+    if not "." in filename: # si el archivo no cuenta con un punto en su nombre quiere decir que no es un archivo y retornara falso 
         return False
 
     ext = filename.rsplit(".", 1)[1]
 
-    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-        return True
-    else:
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:# verificamos si la extension del archivo se encuentra en la extensiones permitidas en la aplicacion
+        return True # si es asi retornara verdadero y nos dejara seguir 
+    else: # en caso contrario no es un archivo permitido y no dejará subirlo 
         return False
+"""
+se define la ruta /upload-image que utiliza el metodo Http POST  en donde vamos a recibir de nuestro formulario
+la imagen que el usuario desea converti y la extension a la que se desea pasar  este evaluar si la imagen evaluara si es valida o no. si lo es
+la guardara temporalmente en una carpeta predefinida, luego llamara el metodo de convertir imagen mandano como parametro la imagen que se recibio
+y la nueva extension, se convierte de manera exitosa se retornara la nueva imagen a la ruta /download-image pasando como parametro la nueva imagen.
 
+"""
 @app.route("/upload-image", methods = ["POST"])
 def recibir_imagen():
     if request.method == "POST":
@@ -42,10 +48,10 @@ def recibir_imagen():
             else:
                 return redirect(url_for('index', error = "Archivo no valido"))
 
-@app.route("/download-image/<nombre_imagen>")
-def descargar_imagen(nombre_imagen):
+@app.route("/download-image/<nombre_imagen>")# se define la ruta /download-image que recibira como parametro una imagen 
+def descargar_imagen(nombre_imagen):# recibe el nombre de una imagen como parametro 
     try:
-        return send_from_directory(app.config["IMAGE_CONVERTED"], filename=nombre_imagen, as_attachment=True)
-    except FileNotFoundError:
+        return send_from_directory(app.config["IMAGE_CONVERTED"], filename=nombre_imagen, as_attachment=True)# si el nombre de imagen se encuentra en la carpeta de imagenes compartidas se retornara esta imagen para su descarga
+    except FileNotFoundError: # En dado caso que no se encuentre uma imagen con es nombre se retornara un Error 
         abort(404)
               
